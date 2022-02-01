ad9c58a5a8e8ffbbd3989409840471635b6626c1
Quinn K <quinnkass@gmail.com>
2022-01-31 20:43:25 -0500

    add heroku

--- usersite/settings.py
+++ usersite/settings.py
@@ -1,3 +1,9 @@
+import os
+import django_heroku
+import dj_database_url
+from decouple import config
+
+
 """
 Django settings for usersite project.
 
@@ -48,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'usersite.urls'
@@ -118,7 +125,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
